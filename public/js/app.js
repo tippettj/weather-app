bbc5782a0202ad70010dad58d3d9fcc25d67eb3e
tippettj <tippettj@gmail.com>
2020-09-11 11:10:20 -0700

    Remove comments

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log(" Client side Javascript file loaded");
-
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 // # querries by id, . querries by classname
